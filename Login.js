4badd34f2dcec6bb6e2ea97a10223501945b69c0
Dilhan Saliya Bandara Samarakoon <112448310+Dilhan234@users.noreply.github.com>
2023-11-01 09:56:46 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Add your authentication logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
